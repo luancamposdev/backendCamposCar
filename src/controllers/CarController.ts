2b1ac31bd5c7a269beca87549d324aa2e1706902
Luan Campos <luancamposproducoes@gmail.com>
2021-10-29 12:04:45 -0300

    create file upload

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -7,23 +7,21 @@ class CarController {
   async create(request: Request, response: Response) {
     const carRepository = getCustomRepository(CarRepository)
 
-    const { name, marca, modelo, price, avatar } = request.body
+    return response.json({ data: request.body, avatar: request.file })
 
-    console.log(avatar)
-
-    const car = carRepository.create({
-      name,
-      marca,
-      modelo,
-      price,
-      avatar,
-    })
+    // const car = carRepository.create({
+    //   name,
+    //   marca,
+    //   modelo,
+    //   price,
+    //   avatar,
+    // })
 
     // await carRepository.save(car)
 
     // delete user.password;
 
-    return response.status(201).json(car)
+    return response.status(201).json({ message: 'algo' })
   }
 }
 
